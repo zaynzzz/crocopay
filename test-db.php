8f66a68498147ce352d3f808f481b74b9e6f32b8
zaynzzz <89463457+zaynzzz@users.noreply.github.com>
2025-08-02 22:18:37 +0700

    Update test-db.php untuk keperluan pengujian lokal

--- test-db.php
+++ test-db.php
@@ -1,14 +1,1 @@
-<?php
-$conn = new mysqli(
-    'localhost',
-    'root',
-    'root',
-    'croco_pay',
-    3306,
-    '/Applications/MAMP/tmp/mysql/mysql.sock'
-);
-
-if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
-}
-echo "Connected successfully!";
+// ini untuk testing ready to deploy
